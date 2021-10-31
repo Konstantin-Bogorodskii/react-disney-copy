c1c11579b94100f3d6e106648b24b2dff4712302
kbogorodsky <konstantin.bogorodsky@gmail.com>
2021-10-31 23:52:40 +0300

    started creating Login component + styles / added fonts for project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './components/Login';
 
 function App() {
-  return <div>asdasd</div>;
+  return (
+    <div>
+      <Switch>
+        <Route path="/" exact>
+          <Login />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
