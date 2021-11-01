0510127c5bda3d8cc41cc3fa8ddad4a9e66c746b
kbogorodsky <konstantin.bogorodsky@gmail.com>
2021-11-01 22:16:19 +0300

    added images/videos for project / created Login component + styles

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -4,7 +4,19 @@ import styled from 'styled-components';
 function Login() {
   return (
     <Container>
-      <Content>Content</Content>
+      <Content>
+        <CTA>
+          <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
+          <SignUp>GET ALL THERE</SignUp>
+          <Description>
+            Get Premier Access to Raya and the Last Dragon for an additional fee with a Disney+
+            subscription. As of 03/26/21, the price of Disney+ and The Disney Bundle will increase
+            by $1.
+          </Description>
+          <CTALogoTwo src="/images/cta-logo-two.png" alt="" />
+        </CTA>
+        <BgImage />
+      </Content>
     </Container>
   );
 }
@@ -28,6 +40,74 @@ const Content = styled.div`
   align-items: center;
   padding: 80px 40px;
   height: 100%;
+`;
+
+const BgImage = styled.div`
+  height: 100%;
+  background-image: url('./images/login-background.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: -1;
+`;
+
+const CTA = styled.div`
+  margin-bottom: 2vw;
+  max-width: 650px;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  justify-content: center;
+  margin-top: 0;
+  align-items: center;
+  margin-right: auto;
+  margin-left: auto;
+  transition: opacity 0.3s ease-out;
+  width: 100%;
+`;
+const CTALogoOne = styled.img`
+  margin-bottom: 12px;
+  max-width: 600px;
+  min-height: 1px;
+  display: block;
+  width: 100%;
+`;
+
+const CTALogoTwo = styled.img`
+  margin-bottom: 20px;
+  max-width: 600px;
+  display: inline-block;
+  vertical-align: bottom;
+  width: 100%;
+`;
+
+const SignUp = styled.a`
+  font-weight: 700;
+  color: #f9f9f9;
+  background-color: #0063e5;
+  margin-bottom: 12px;
+  width: 100%;
+  letter-spacing: 1.5px;
+  font-size: 18px;
+  padding: 16.5px 0;
+  border: 1px solid transparent;
+  border-radius: 5px;
+  transition: background-color 0.2s ease-in-out;
+
+  &:hover {
+    background-color: #0483ee;
+  }
+`;
+
+const Description = styled.p`
+  color: hsla(0, 0%, 95.3%, 1);
+  font-size: 12px;
+  margin: 0 0 24px;
+  line-height: 1.5;
+  letter-spacing: 1.5px;
 `;
 
 export default Login;
