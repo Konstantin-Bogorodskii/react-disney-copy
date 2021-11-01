15716d02fd204101debf4dbf948dd9df73807763
kbogorodsky <konstantin.bogorodsky@gmail.com>
2021-11-01 23:29:15 +0300

    started header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
 import Login from './components/Login';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route path="/" exact>
           <Login />
