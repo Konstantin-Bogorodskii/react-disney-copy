9f87cd843a1973e8d0c5f3426840e7d36a5d047a
kbogorodsky <konstantin.bogorodsky@gmail.com>
2021-11-06 23:02:13 +0300

    get tmbd API key

---
+++ src/api/api.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const API_KEY_3 = 'e893d153c2534d5eb3897a5aae50157a';
+
+export const API_KEY_4 =
+  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlODkzZDE1M2MyNTM0ZDVlYjM4OTdhNWFhZTUwMTU3YSIsInN1YiI6IjYxODZkNTYzNjlkMjgwMDA0NDk1NzEzMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wL1E3kosdjm4Sb9-3xd6UK0gb4w5prta_onZr6WAqV0';
